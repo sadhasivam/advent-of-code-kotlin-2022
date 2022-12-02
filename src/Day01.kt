b877b40ca8fb58f3c4c8ff781b3a79e3dfafe246
sadhasivam jayabalaganesan <sadha@shoprunner.com>
2022-12-01 23:00:40 -0500

    aoc-day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,60 @@
+import java.io.File
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    val maxNCalories = 3
+
+    fun computeElvesCaloriesList(input: List<String>): List<Int> {
+        val elvesCalories = mutableListOf<Int>()
+        var calories = 0
+        input.forEach {
+            if (it.trim() != "") {
+                calories += it.toInt()
+            } else {
+                elvesCalories.add(calories)
+                calories = 0
+            }
+        }
+        if (calories > 0) {
+            elvesCalories.add(calories)
+        }
+        return elvesCalories
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    /**
+     * Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+     */
+    fun maxElvesCalories(input: List<String>): Int =
+        computeElvesCaloriesList(input).max()
 
+    /**
+     * Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+     */
+    fun topNElvesCalories(input: List<String>, nCalories: Int): Int =
+        computeElvesCaloriesList(input)
+            .sorted()
+            .asReversed()
+            .take(nCalories)
+            .sum()
+
+
+    // Alternate Idea
+    val data = File("src/Day01_test.txt").readText()
+    val s = data.split("\n\n").map { elf ->
+        elf.lines().map { it.toInt() }.sum()
+    }
+    s.max()
+    
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println("checking test input for Max Elf calories ")
+    check(maxElvesCalories(testInput) == 24000)
+    println("checking test input for Max top 3 Elves calories ")
+    check(topNElvesCalories(testInput, maxNCalories) == 45000)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("checking test input for Max Elf calories ")
+    check(maxElvesCalories(input) == 67633)
+    println("checking test input for Max top 3 Elves calories ")
+    check(topNElvesCalories(input, maxNCalories) == 199628)
 }
